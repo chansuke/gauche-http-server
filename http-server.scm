23629bd4ea52ba4d29a39d751097cfe095bbff6d
chansuke <moonset20@gmail.com>
2014-11-21 15:49:06 +0900

    add main args

--- http-server.scm
+++ http-server.scm
@@ -1,10 +1,18 @@
+(use file.util)
+(use gauche.fcntl)
+(use gauche.interactive)
 (use gauche.net)
-(use util.match)
+(use gauche.selector)
 (use rfc.822)
 (use rfc.url)
 (use text.tree)
 (use text.html-lite)
 (use www.cgi)
+
+;;; add main args
+(define (main args)
+  (run-server)
+  0)
 
 ;;; main loop function of http server
 (define (run-server)
@@ -25,6 +33,7 @@
      (list* meth abs-path (rfc822-read-headers iport))]
     [#/^[A-Z]+/ () 'not-implemented]
     [else 'bad-request]))
+
 ;;; create http-response
 (define (handle-request request oport)
   (match request
@@ -39,3 +48,16 @@
                 (display #`"Content-Length: ,(string-size content)\r\n" oport)
                 (display "\r\n" oport)
                 (when (equal? meth "GET") (display content oport))))])
+
+;;; create contents
+(define (render-content path params)
+  (tree->string
+    (html:html
+      (html:head (html:title "httpd written by gauche"))
+      (html:body (html:h1 "This is simple httpd by Gauche")
+                 (html:p "Path : " (html-escape-string path))
+                 (map (lambda (p)
+                        (html:p (html-escape-string (car p)) " : "
+                                (html-escape-string (cdr p))))
+                      params)))))
+
